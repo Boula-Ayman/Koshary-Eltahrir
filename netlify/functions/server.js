896c9ae4c8d04a5fc3b47d321d5e347d6cf230ec
Boula-Ayman <boulaaymanzaher@gmail.com>
2025-09-22 16:38:49 +0300

    net

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,11 +1,30 @@
 const { createRequestHandler } = require("@react-router/node");
 
-module.exports = {
-  async fetch(request) {
-    const handler = createRequestHandler({
-      build: require(process.cwd() + "/build/server/index.js"),
-    });
+exports.handler = async (event, context) => {
+  // Convert Netlify event to Fetch API Request
+  const url = new URL(event.rawUrl || event.url);
+  const request = new Request(url.toString(), {
+    method: event.httpMethod,
+    headers: event.headers,
+    body: event.body
+      ? Buffer.from(event.body, event.isBase64Encoded ? "base64" : "utf8")
+      : undefined,
+  });
 
-    return handler(request);
-  },
+  const handler = createRequestHandler({
+    build: require(process.cwd() + "/build/server/index.js"),
+  });
+
+  const response = await handler(request);
+
+  // Convert Fetch API Response to Netlify response format
+  const responseBody = await response.text();
+  const headers = Object.fromEntries(response.headers.entries());
+
+  return {
+    statusCode: response.status,
+    headers,
+    body: responseBody,
+    isBase64Encoded: false,
+  };
 };
