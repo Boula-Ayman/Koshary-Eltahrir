ef834ddd80d1f3d2ca60ae1209be2f72ef15a84c
Boula-Ayman <boulaaymanzaher@gmail.com>
2025-09-22 16:35:37 +0300

    net

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -3,7 +3,7 @@ const { createRequestHandler } = require("@react-router/node");
 module.exports = {
   async fetch(request) {
     const handler = createRequestHandler({
-      build: require("./../build/server/index.js"),
+      build: require(process.cwd() + "/build/server/index.js"),
     });
 
     return handler(request);
